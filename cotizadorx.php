dc1606e9c0fdd2142852ae168c866a1d1cb79348
Ari4ke <80093586+Ari4ke@users.noreply.github.com>
2023-03-22 12:35:30 +0100

    icon icon icon icon

--- cotizadorx.php
+++ cotizadorx.php
@@ -88,14 +88,18 @@ function get_precio_por_zip($zip, $peso,$largo){
                     $tarifas_x_zonas = get_post_meta($zona_post->ID, 'tarifa_por_peso','DESC' );
                     /*Deberemos implantar un código que detecte el código postal donde nos ubicamos para poder aceptarlo*/
                     $tarifa_zona_bool = true;
-                    //var_dump("Código postal detectado" .$zip. "<br>Extrayendo los códigos postales de la " .$zona_ubi. ": " .$postmeta_cp. "<br>Donde vamos a extraer las siguientes tarifas de la zona correspondiente: " .$tarifas_x_zonas);
+                    var_dump("Código postal detectado: " .$zip. "<br>Extrayendo los códigos postales de la " .$zona_ubi. ": " .$postmeta_cp. "<br>Donde vamos a extraer las siguientes tarifas de la zona correspondiente: " .$tarifas_x_zonas);
                     break; // salimos del loop si encontramos una zona válida
                 }else{
-                    $tarifa_zona_bool = false;
-                    //var_dump("<br>Lo sentimos pero aunque haya pasado los filtros o este CP no existe o has introducido losdatos incorrectamente"); /*BORRAR*/
+                    $tarifa_zona_bool = false; 
                 }
 
-            }     
+            }
+            if(!$tarifa_zona_bool){
+                echo "<h1>Vaya! Parece que este código postal no existe o es incorrecto</h1>";
+                header('Refresh: 3; url = https://sctrans.es/cotizador/');
+                exit();
+            }
     }
     // buscar la localidad
     if(!in_array($prefijo_cp, $prefijo_x_zonas)){
@@ -120,8 +124,8 @@ function get_precio_por_zip($zip, $peso,$largo){
 
     // Obtener array de tarifas de la localidad seleccionada o de la zona seleccionada
 
-    //var_dump("<br>".$localidad->post_title."<-Esta es la comunidad asignada y sus datos->".$tarifas_string);
-    //var_dump("Valencia->".$tarifas_x_zonas);
+    var_dump("<br>".$localidad->post_title."<-Esta es la comunidad asignada y sus datos->".$tarifas_string);
+    var_dump("Valencia->".$tarifas_x_zonas);
 
     if(empty($tarifas_string)) {
         $tarifas = explode("\n", $tarifas_x_zonas );
@@ -148,23 +152,16 @@ function get_precio_por_zip($zip, $peso,$largo){
     // Obtener el precio segun el peso    
     foreach ($tarifas_formateada as $tarifa) {        
         if ($peso >= $tarifa['peso_inicial'] && $peso <= $tarifa['peso_final'] ) {
-            //////var_dump("entro acá");
             if($largo>3 && $prefijo_cp=='28'){
-                //var_dump("entro acá 222");
                $tarifa_base = (float)$tarifa['precio']+((float)$tarifa['precio']*0.25);
             }else{
-                //var_dump("entro acá 1.1");
                 if($largo>2.4 && $prefijo_cp=='04'){
-                    //var_dump("entro acá 3.0");
                     $tarifa_base = (float)$tarifa['precio']+((float)$tarifa['precio']*0.25);
                 }else{
                    if($largo>3.5 && $prefijo_cp=='03'){
-                    //var_dump("entro acá 4000");
                       $tarifa_base = (float)$tarifa['precio']+((float)$tarifa['precio']*0.25);
                    }else{
-                    //var_dump("entro acá 500");
                       $tarifa_base = $tarifa['precio'];
-                    //var_dump("TARIFA BASE -> ". $tarifa_base);
                    }
                 }
             }            
@@ -266,7 +263,7 @@ function send_add_contratar(){
                    $alto=$value->meta_value;
                 break;
                  case 34:
-                   $rec_comb=$value->meta_value; 
+                   $rec_comb=$value->meta_value;   
                 break;
         }
     }
@@ -297,168 +294,75 @@ function send_add_contratar(){
        $total = $precio_base + $valor_envio_aereo + $valor_plataforma_elevadora + $valor_mercancia_peligrosa+$importe_por_mozo_hora; 
     }
     
+   
     $sql = " UPDATE ".$wpdb->prefix."gf_entry_meta ".
            " set meta_value='contratado' ".
            " WHERE `form_id` = 1 AND `entry_id` =".$nro_cotizacion." and meta_key='32' ";
     $wpdb->get_results($sql);    
-    $html ='<!DOCTYPE html>
-            <html>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <style>
-                body {
-                    padding-right: 15%;
-                    padding-left: 15%;
-                }
-            
-                /* media query para pantallas más pequeñas (móviles) */
-                @media (max-width: 480px) {
-                    body {
-                        padding-right: 0px;
-                        padding-left: 0px;
-                    }
-                }
-
-                .tableInside{
-                    display:flex; 
-                    flex-flow: row wrap;
-                    justify-content: space-between;
-                }
-
-                .table1, .table2{
-                    width: 45%;
-                }
-
-                @media (max-width: 480px) {
-                    .table1, .table2 {
-                        width: 100%;
-                    }
-                }
-
-                b{
-                    font-size: 13px;
-                }
-
-                tr{
-                    border-bottom: solid 5px white;
-                }
-                </style>
-            </head>
-            <body>'.
-    '<div class="confirmacion" style="padding-right: 5%; padding-left: 5%;">'.
-           '<img class="logo-Sctrans" src="https://sctrans.es/wp-content/uploads/2021/03/sctrans-positivo-color.png" style="display: block;margin-left: auto;margin-right: auto; width: 50%;"/>
-            <h2 style="text-align: center; margin-top: 40px; font-weight: 10px;">Muchas gracias por contratar nuestro servicio.</h2>
-            <h3 style="text-align: center; font-size: 15px;">Los datos de su cotización se encuentran en
-            <div style="text-align: center; margin-top: 10px; font-size: 18px;">'.do_shortcode('[gravitypdf id="61c9ff718c51c"  entry="'.$nro_cotizacion.'" text="Descargar PDF Cotización"]').'</div>
-            <h4>Su cotización fue generada exitosamente. Esta cotización está sujeta a los siguientes <a href="'.site_url().'/condiciones-generales-transporte-terrestre/" target="_blank">términos y condiciones</a>.</h4>
+    $html ='<div class="confirmacion" >'.
+           '<p>Muchas gracias por contratar nuestro servicio. La cotización generada tiene como referencia el #'.$nro_cotizacion.'.<br> 
+            Para encontrar nuestros diferentes canales de contacto en nuestra página <a href="https://sctrans.es/contacto-y-cotizacion/"traget="_blank">https://sctrans.es/contacto-y-cotizacion/</a> para obtener información sobre su servicio.También puede revisar  sobre las condiciones generales del transporte terrestre aquí  <a href="https://sctrans.es/condiciones-generales-transporte-terrestre/" target="_blank">https://sctrans.es/condiciones-generales-transporte-terrestre/ </a></p>'.
+           '<p><b><label>Direccion de Recogida: </label></b>'.$dir_recogida.'</p>'.
+           '<p><b><label>Direccion de Entrega: </label></b>'.$dir_entrega.'</p>'.
+           '<p><b><label>Referencia de Recogida: </label></b>'.$ref_recogida.'</p>'.
+           '<p><b><label>Referencia de Entrega: </label></b>'.$ref_entrega.'</p>'.
+           '<h4>Cotización #'.$nro_cotizacion.'</h4>'.
+           '<p>Su cotización fue generada exitosamente. Esta cotización está sujeta a los siguientes términos y condiciones que se enunciona <a href="'.site_url().'/condiciones-generales-transporte-terrestre/" target="_blank">aquí</a></p>
             <div class="detalle-cotizacion">
-            <h2 class=" text-align: center; margin-top: 20px;">Cotización #'.$nro_cotizacion.'</h2>
-            <div class="tableInside">
-                <table class="table1" style="margin-bottom: 35px;">
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>Cliente:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$nombre_user.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>C.P. Origen:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$cod_postal_origen.'</td>  
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>C.P. Destino:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$cod_postal_destino.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>Paquetes:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$paquetes.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>Peso:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$peso.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>Volumen:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$volumen.'m<sup>3</sup></td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff;"><b>Mozo/hora:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42;">'.$valor_importe_mozo.'</td>
-                    </tr>
-                </table>
-
-                <table class="table2" style="margin-bottom: 35px;">
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff; text-align: center;"><b>Direccion de Recogida:</b></td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;">'.$dir_recogida.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff; text-align: center;"><b>Direccion de Entrega:</b></td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;">'.$dir_entrega.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff; text-align: center;"><b>Referencia de Recogida:</b></td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;">'.$ref_recogida.'</td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #0A5687; color: #fff; text-align: center;"><b>Referencia de Entrega:</b></td>
-                    </tr>
-                    <tr>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;">'.$ref_entrega.'</td>
-                    </tr>
-                </table>
-            </div>
-            <table style=" padding: 5px; margin: 8px auto; font-size: 16px; width: 100%; margin-bottom: 30px;">
+            <p><b><label>Cliente: </label></b><span class="data-cliente">'.$nombre_user.'</span></p>
+            <p><b><label>Cod.Postal Origen: </label></b><span class="data-zip-origen">'.$cod_postal_origen.'</span></p>
+            <p><b><label>Cod.Postal Destino:</label></b><span class="data-zip-destino">'.$cod_postal_destino.'</span></p>
+            <p><b><label>Paquetes: </label></b><span class="data-paquetes">'.$paquetes.'</span></p>
+            <p><b><label>Peso: </label></b><span class="data-peso">'.$peso.'</span>kg.</p>
+            <p><b><label>Volumen: </label></b><span class="data-volumen">'.$volumen.'</span>m3</p>
+            <p><b><label>Mozo/Hora: </label></b><span class="data-volumen">'.$valor_importe_mozo.'</span>hora</p>
+            <table>
+                <thead>
+                    <tr><th>Descripción</th><th>Total</th></tr>
+                </thead>
                 <tbody>
-                    <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Precio Base</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_precio_base">'.$precio_base.'€</span></td>
+                    <tr><td><b>Precio Base</b></td>
+                        <td><span id="valor_precio_base">'.$precio_base.'€</span></td>
                     </tr>
-                    <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Envío Aereo</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_envio_aereo">'.$valor_envio_aereo.'€</span></td>
+                    <tr><td><b>Envío Aereo</b></td>
+                        <td><span id="valor_envio_aereo">'.$valor_envio_aereo.'€</span></td>
                     </tr>
-                    <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Plataforma Elevadora</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_plataforma_elevadora">'.$valor_plataforma_elevadora.'€</span></td>
+                    <tr><td><b>Plataforma Elevadora</b></td>
+                        <td><span id="valor_plataforma_elevadora">'.$valor_plataforma_elevadora.'€</span></td>
                     </tr>
-                    <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Mercancía Peligrosa</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_mercancia_peligrosa">'.$valor_mercancia_peligrosa.'€</span></td>
+                    <tr><td><b>Mercancía Peligrosa</b></td>
+                        <td><span id="valor_mercancia_peligrosa">'.$valor_mercancia_peligrosa.'€</span></td>
                     </tr>';
     if($mercancia_peligrosa=="Si"){
-        $html .='   <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Recargo del combustible</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_recargo_combustible">'.$valor_rec_comb.'€</span></td>
+        $html .='   <tr><td><b>Recargo del combustible</b></td>
+                        <td><span id="valor_recargo_combustible">'.$valor_rec_comb.'€</span></td>
                     </tr>';
     }                
-    $html .='       <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b>Peonaje por Hora/Mozo</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="importe_por_mozo_hora">'.$importe_por_mozo_hora.'€</span></td>
+    $html .='       <tr><td><b>Peonaje por Hora/Mozo</b></td>
+                        <td><span id="importe_por_mozo_hora">'.$importe_por_mozo_hora.'€</span></td>
                     </tr>
                 </tbody>
                 <tfoot>
-                    <tr><td style="padding: 15px; width: 190px; background: #0A5687; color: #fff;"><b style="font-size: 18px;">TOTAL:</b></td>
-                        <td style="padding: 15px; background: #F9F6F0; color: #020f42; text-align: center;"><span id="valor_total"><b style="font-size: 16px;">'.number_format($total, 2).'€</b></span></td>
+                    <tr><td><b>TOTAL</b></td>
+                        <td><span id="valor_total">'.$total.'€</span></td>
                     </tr>
                 </tfoot>
             </table>
             </div>
-            <p style="text-align: justify;">Para encontrar nuestros diferentes canales de contacto en nuestra página <a href="https://sctrans.es/contacto/"target="_blank">https://sctrans.es/contacto/</a> para obtener información sobre su servicio.</p>
-            <p style="text-align: justify;">También puede revisar  sobre las condiciones generales del transporte terrestre aquí  <a href="https://sctrans.es/condiciones-generales-transporte-terrestre/" target="_blank">https://sctrans.es/condiciones-generales-transporte-terrestre/ </a></p>
-            <p style="text-align: justify;">Si usted tiene alguna pregunta sobre esta cotización, por favor, póngase en contacto con nosotros. </p>        
+            <p>Si usted tiene alguna pregunta sobre esta cotización, por favor, póngase en contacto con nosotros.</p>        
         </div>';
     /*$headers = array(
         //'From: Me Myself <[email protected]>',
         'content-type: text/html',
         //'Cc: John Q Codex <[email protected]>',
         //'Cc: [email protected]',
-    );*/  
-
+    );*/    
     $usuario = get_userdata (get_current_user_id());
     $usuario_email=$usuario->user_email;
-    wp_mail($email,"SC Trans -  Servicio Contratado",$html,$headers, $attachments);
+    wp_mail($email,"SC Trans -  Servicio Contratado",$html,$headers);
     //$email="rodrigolopez2012187@gmail.com";
     //wp_mail($email,"SC Trans -  Servicio Contratado",$html,$headers);
-    wp_mail($usuario_email,"SC Trans -  Servicio Contratado",$html,$headers, $attachments);
+    wp_mail($usuario_email,"SC Trans -  Servicio Contratado",$html,$headers);
 
     
 
